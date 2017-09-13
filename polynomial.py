f849421973cb26c40e929ccfd2092dedee57bef7
notsoimp <poroshinaan@gmail.com>
2017-09-13 14:36:44 +0500

    start

---
+++ polynomial.py
@@ -0,0 +1,91 @@
+import re
+
+
+class Polynomial:
+    def __init__(self, polynom):
+        self._polynom = polynom.replace(' ', '')
+        self.errors = ''
+        self._variables = list(set(re.findall(r'[a-zA-Z]', polynom)))
+        self._variables.sort()
+        self._dict = {}
+        self._variable_degree_dict = {x: '' for x in self._variables}
+
+    def is_correct(self):
+        unacceptables = re.findall(r'[^a-zA-Z\d\-+*^()]', self._polynom)
+        if not unacceptables.__len__() == 0:
+            self.errors = 'unacceptable symbols: '+' '.join(x for x in unacceptables)
+            return False
+        pre_hats = re.findall(r'(?<![a-zA-Z])\^', self._polynom)
+        if not pre_hats.__len__() == 0:
+            self.errors = 'strange symbol before \"^\". ' \
+                           '\nRemember that I can not build a number to the power'
+            return False
+        after_hats = re.findall(r'\^(?!\d)', self._polynom)
+        if not after_hats.__len__() == 0:
+            self.errors = 'strange symbol after \"^\".' \
+                           '\nRemember that I can work only with natural degrees'
+            return False
+        open_brace = re.findall(r'[(]', self._polynom)
+        close_brace = re.findall(r'[)]', self._polynom)
+        if len(open_brace) != len(close_brace):
+            self.errors = 'opened and closed braces'
+            return False
+        return True
+
+    def split_polynom_to_dict(self):
+        self._polynom = self._polynom.replace('-', '+-')
+        monomials = self._polynom.split('+')
+        for monom in monomials:
+            variable_degree_dict = self.init_var_deg_dict(monom)
+            for var in self._variables:
+                if variable_degree_dict[var] == '':
+                    variable_degree_dict[var] = 0
+                variable_degree_dict[var] = int(variable_degree_dict[var])
+            variable_degree_dict['_'] = int(variable_degree_dict['_'])
+            degrees_tup = tuple(variable_degree_dict[e] for e in self._variables)
+            if self._dict.get(degrees_tup) is None:
+                self._dict[degrees_tup] = variable_degree_dict['_']
+            else:
+                self._dict[degrees_tup] += variable_degree_dict['_']
+        return self._dict
+
+    def init_var_deg_dict(self, monom):
+        variable_degree_dict = {x: '' for x in self._variables}
+        variable = ''
+        degree = ''
+        multiplier = ''
+        for index, e in enumerate(monom):
+            if e == '*' or e == '^':
+                continue
+            if variable == '':
+                if e.isalpha():
+                    variable = e
+                    if multiplier == '':
+                        multiplier = '1'
+                    elif multiplier == '-':
+                        multiplier = '-1'
+                    variable_degree_dict['_'] = multiplier
+                else:
+                    multiplier += e
+            elif index == len(monom) - 1:
+                if e.isalpha():
+                    variable_degree_dict[variable] = degree if not degree == '' else '1'
+                    variable_degree_dict[e] = '1'
+                elif e.isdigit():
+                    degree += e
+                    variable_degree_dict[variable] = degree
+                    degree = ''
+            elif e.isalpha():
+                if degree == '':
+                    degree = '1'
+                variable_degree_dict[variable] = degree
+                variable = e
+                degree = ''
+            else:
+                degree += e
+        return variable_degree_dict
+
+    def is_equal(self, other):
+        parsed1 = self.split_polynom_to_dict()
+        parsed2 = other.split_polynom_to_dict()
+        return parsed1 == parsed2
---
+++ tests.py
@@ -0,0 +1,39 @@
+import unittest
+from polynomial import Polynomial
+
+
+class TestParsers(unittest.TestCase):
+    def test_is_correct(self):
+        self.assertTrue(Polynomial.is_correct(Polynomial('2x^2 + 3xy')))
+        self.assertTrue(Polynomial.is_correct(Polynomial('2 * x ^ 2')))
+        self.assertTrue(Polynomial.is_correct(Polynomial('(15x)(13z^3+2)')))
+        self.assertFalse(Polynomial.is_correct(Polynomial('(15x')))
+        self.assertFalse(Polynomial.is_correct(Polynomial('25^x')))
+        self.assertFalse(Polynomial.is_correct(Polynomial('x^*5')))
+
+    def test_init_dict(self):
+        self.assertEqual(Polynomial.init_var_deg_dict(Polynomial('2xy'), '2xy'),
+                         {'_': '2', 'x': '1', 'y': '1'})
+        self.assertEqual(Polynomial.init_var_deg_dict(Polynomial('2xy+3z'), 'x^2y'),
+                         {'_': '1', 'x': '2', 'y': '1', 'z': ''})
+        self.assertEqual(Polynomial.init_var_deg_dict(Polynomial('x^3*y^3'), 'x^3*y^3'),
+                         {'x': '3', 'y': '3', '_': '1'})
+        self.assertEqual(Polynomial.init_var_deg_dict(Polynomial('-x^2'), '-x^2'),
+                         {'x': '2', '_': '-1'})
+
+    def test_split_polynom(self):
+        self.assertEqual(Polynomial.split_polynom_to_dict(Polynomial('2xy')),
+                         {(1, 1): 2})
+        self.assertEqual(Polynomial.split_polynom_to_dict(Polynomial('2x^3y^4')),
+                         {(3, 4): 2})
+        self.assertEqual(Polynomial.split_polynom_to_dict(Polynomial('x^3+ 15x^8')),
+                         {(3,): 1, (8,): 15})
+        self.assertEqual(Polynomial.split_polynom_to_dict(Polynomial('15x^10*y^3 - z^4')),
+                         {(10, 3, 0): 15, (0, 0, 4): -1})
+
+# TODO
+# class TestMultiplication(unittest.TestCase):
+
+
+if __name__ == '__main__':
+    unittest.main()
